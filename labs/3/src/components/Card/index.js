868bc001eff33d67c6092952207b033faa78e92b
S-Billings <shammymeatsix@gmail.com>
2020-02-21 14:05:02 -0500

    I think I am done...

--- labs/3/src/components/Card/index.js
+++ labs/3/src/components/Card/index.js
@@ -1,16 +1,13 @@
-// Import correct component
-import Card from "./component/card/index.js";
+import React from "react";
+import "./index.css";
 
-// This creates a reference to the Card Class
-
-function runOnLoad(){
-    // Create a container for us
-        let element = document.createElement("div");    
-        element.id = "container";    
-        document.body.appendChild(element);    
-        
-        var newTask = new Card({content:"Sample value provided"});    
-        
-        element.appendChild(newTask.render());}
-        
-        window.addEventListener("DOMContentLoaded", runOnLoad);
+class Card extends React.Component {
+    render(){
+        return(
+            <div className = "card">
+                {this.props.content}
+            </div>
+        )
+    }
+}
+export default Card;
