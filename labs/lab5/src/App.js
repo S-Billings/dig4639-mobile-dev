4870050824cc916101d95be92a4facbe0f6165fc
S-Billings <shammymeatsix@gmail.com>
2020-03-27 12:06:34 -0400

    fixes

--- labs/lab5/src/App.js
+++ labs/lab5/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {Card} from 'react-native-elements';
-import Weather from "./Components/Weather/index.js"
+import Weather from "./Components/Weather";
 
 class App extends React.Component{
   render(){
