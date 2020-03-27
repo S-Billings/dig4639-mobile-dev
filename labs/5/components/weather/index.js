5fd6a645e8f5b868b00158797bb97d8b538e44d4
S-Billings <shammymeatsix@gmail.com>
2020-03-27 10:39:50 -0400

    weather API done

---
+++ labs/5/components/weather/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+class Weather extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state= {
+            periods:[]
+        };
+    }
+    componetWorks(){
+        fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast").then(res=>res.json()).then((result)=>{
+            let periods = result.props.periods;
+            
+            this.setState({
+                periods: periods
+            });
+
+        })
+        .catch((error)=>{console.log(error)});
+    }
+    render(){
+        return(
+        <View>{
+            this.state.periods.map((value,index)=>{
+                return <View key={index}>
+                    <Text>
+                        Temperature{value.temperature}{value.temperatureUnit}
+                    </Text>
+
+
+                </View>;
+            })
+            }</View>
+        );
+    }
+}
+export default Weather;
